8f1793ff626931da334d1d96f20dc94706b1348c
Tanvi Deshmukh <tanvideshmukh393@gmail.com>
2026-02-27 21:20:49 +0530

    Methods

---
+++ Java_FullStack/src/ListMethods.java
@@ -0,0 +1,13 @@
+
+import java.util.ArrayList;
+public class ListMethods {
+
+	public static void main(String[] args) {
+		ArrayList<String> arr = new ArrayList<String>();
+		arr.add("Tanvi");
+		arr.add("Shruti");
+		System.out.println(arr);
+		System.out.println(arr.contains("Tanvi"));  //It returns boolean value 
+	}
+
+}
